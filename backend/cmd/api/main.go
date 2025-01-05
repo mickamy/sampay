c0221f6b5119e73522cf62d9106ca21d0547e68c
mickamy <tetsuro@mickamy.com>
2025-01-05 14:55:20 +0900

    chore: init slogger

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
+	"github.com/mickamy/slogger"
+
+	"mickamy.com/sampay/config"
 	"mickamy.com/sampay/internal/api"
 	"mickamy.com/sampay/internal/di"
 )
+
+func init() {
+	cfg := config.Common()
+	slogger.Init(slogger.Config{
+		Level:          cfg.SLoggerLevel(),
+		TrimPathPrefix: cfg.PackageRoot,
+		ContextFieldsExtractor: func(ctx context.Context) []any {
+			return []any{}
+		},
+	})
+}
 
 func main() {
 	infras, err := di.InitInfras()
--- backend/config/common_config.go
+++ backend/config/common_config.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"github.com/caarlos0/env/v11"
+	"github.com/mickamy/slogger"
 )
 
 type Env string
@@ -20,6 +21,21 @@ type CommonConfig struct {
 	Env         Env    `env:"ENV"`
 	LogLevel    string `env:"LOG_LEVEL" envDefault:"info"`
 	PackageRoot string `env:"PACKAGE_ROOT"`
+}
+
+func (c CommonConfig) SLoggerLevel() slogger.Level {
+	switch c.LogLevel {
+	case "debug":
+		return slogger.LevelDebug
+	case "info":
+		return slogger.LevelInfo
+	case "warn":
+		return slogger.LevelWarn
+	case "error":
+		return slogger.LevelError
+	default:
+		return slogger.LevelInfo
+	}
 }
 
 var (
