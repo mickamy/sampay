989da1bc67f0d6641c3d10967bfc0382b85bffd2
mickamy <tetsuro@mickamy.com>
2025-01-05 09:01:35 +0900

    feat: add func to seed

---
+++ backend/internal/cli/db/seed/seed.go
@@ -0,0 +1,12 @@
+package seed
+
+import (
+	"context"
+
+	"mickamy.com/sampay/config"
+	"mickamy.com/sampay/internal/cli/infra/storage/database"
+)
+
+func Do(ctx context.Context, db *database.DB, env config.Env) error {
+	return nil
+}
