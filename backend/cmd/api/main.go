1950e3c6425525e8e13b72f8a6310848438dd326
mickamy <tetsuro@mickamy.com>
2025-01-05 13:41:11 +0900

    feat: implement api.NewServer

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,54 +1,23 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"net/http"
 	"os"
 
-	"buf.build/gen/go/mickamy/sampay/connectrpc/go/auth/v1/authv1connect"
-	authv1 "buf.build/gen/go/mickamy/sampay/protocolbuffers/go/auth/v1"
-	"connectrpc.com/connect"
-	"github.com/rs/cors"
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
+	"mickamy.com/sampay/internal/api"
+	"mickamy.com/sampay/internal/di"
 )
 
 func main() {
-	s := newServer()
+	infras, err := di.InitInfras()
+	if err != nil {
+		fmt.Println("failed to initialize infras:", err)
+		os.Exit(1)
+	}
+	s := api.NewServer(infras)
 
-	fmt.Println("listening on port 8080...")
 	if err := s.ListenAndServe(); err != nil {
 		fmt.Println("failed to start server:", err)
 		os.Exit(1)
 	}
 }
-
-func newServer() *http.Server {
-	api := http.NewServeMux()
-	api.Handle(authv1connect.NewSessionServiceHandler(&SessionService{}))
-
-	// TODO: restrict CORS
-	corsHandler := cors.AllowAll().Handler(api)
-
-	return &http.Server{
-		Addr:    ":8080",
-		Handler: h2c.NewHandler(corsHandler, &http2.Server{}),
-	}
-}
-
-type SessionService struct{}
-
-func (s *SessionService) SignIn(ctx context.Context, req *connect.Request[authv1.SignInRequest]) (*connect.Response[authv1.SignInResponse], error) {
-	panic("implement me")
-}
-
-func (s *SessionService) Refresh(ctx context.Context, req *connect.Request[authv1.RefreshRequest]) (*connect.Response[authv1.RefreshResponse], error) {
-	panic("implement	me")
-}
-
-func (s *SessionService) SignOut(ctx context.Context, req *connect.Request[authv1.SignOutRequest]) (*connect.Response[authv1.SignOutResponse], error) {
-	panic("implement me")
-}
-
-var _ authv1connect.SessionServiceHandler = (*SessionService)(nil)
---
+++ backend/internal/api/server.go
@@ -0,0 +1,60 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"connectrpc.com/connect"
+	"github.com/mickamy/slogger"
+	"github.com/rs/cors"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+
+	"mickamy.com/sampay/internal/api/interceptor"
+	"mickamy.com/sampay/internal/di"
+	authRouter "mickamy.com/sampay/internal/domain/auth/router"
+)
+
+func NewServer(infras di.Infras) http.Server {
+	api := http.NewServeMux()
+
+	interceptors := connect.WithInterceptors(
+		interceptor.Logging(),
+	)
+
+	for _, route := range []func(mux *http.ServeMux, infras di.Infras, options ...connect.HandlerOption){
+		authRouter.Route,
+	} {
+		route(api, infras, interceptors)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		if err := infras.Writer.Exec("SELECT 1").Error; err != nil {
+			slogger.ErrorCtx(r.Context(), "failed to ping database", "err", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if err := infras.Reader.Exec("SELECT 1").Error; err != nil {
+			slogger.ErrorCtx(r.Context(), "failed to ping database", "err", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if err := infras.KVS.Ping(context.Background()).Err(); err != nil {
+			slogger.ErrorCtx(r.Context(), "failed to ping kvs", "err", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+
+	mux.Handle("/api/", http.StripPrefix("/api", api))
+
+	corsHandler := cors.AllowAll().Handler(mux)
+
+	return http.Server{
+		Addr:    fmt.Sprintf(":%d", 8080),
+		Handler: h2c.NewHandler(corsHandler, &http2.Server{}),
+	}
+}
