9e6e3ba54b1901d0ecbcbcebd82d040c251ee27a
mickamy <tetsuro@mickamy.com>
2025-01-05 21:15:30 +0900

    feat: add i18n interceptor

---
+++ backend/internal/api/interceptor/i18n_interceptor_test.go
@@ -0,0 +1,66 @@
+package interceptor_test
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"buf.build/gen/go/mickamy/sampay/connectrpc/go/test/v1/testv1connect"
+	testv1 "buf.build/gen/go/mickamy/sampay/protocolbuffers/go/test/v1"
+	"connectrpc.com/connect"
+	"github.com/stretchr/testify/require"
+
+	"mickamy.com/sampay/internal/api/interceptor"
+	"mickamy.com/sampay/internal/domain/test/handler"
+	"mickamy.com/sampay/internal/lib/contexts"
+	"mickamy.com/sampay/internal/lib/language"
+	"mickamy.com/sampay/internal/lib/ptr"
+)
+
+type mockHandler struct{}
+
+func (m *mockHandler) Handle(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
+	return connect.NewResponse[string](ptr.Of("mock response")), nil
+}
+
+func TestI18N(t *testing.T) {
+	t.Parallel()
+
+	tsc := []struct {
+		name         string
+		acceptLang   string
+		expectedLang language.Type
+	}{
+		{name: "success", acceptLang: "ja", expectedLang: language.Japanese},
+		{name: "success (not supported language)", acceptLang: "en", expectedLang: language.Japanese},
+		{name: "success (empty header)", acceptLang: "", expectedLang: language.Japanese},
+		{name: "success (invalid language format)", acceptLang: "test", expectedLang: language.Japanese},
+	}
+
+	for _, tc := range tsc {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			// arrange
+			test := func(ctx context.Context, req *connect.Request[testv1.TestRequest]) {
+				lang := contexts.MustLanguage(ctx)
+				require.Equal(t, tc.expectedLang, lang)
+			}
+			mux := http.NewServeMux()
+			sut := interceptor.I18N()
+			interceptors := connect.WithInterceptors(sut)
+			mux.Handle(testv1connect.NewTestServiceHandler(&handler.TestHandler{Exec: test}, interceptors))
+			server := httptest.NewServer(mux)
+			defer server.Close()
+
+			// act
+			client := testv1connect.NewTestServiceClient(http.DefaultClient, server.URL)
+			_, err := client.Test(context.Background(), connect.NewRequest(&testv1.TestRequest{}))
+
+			// assert
+			require.NoError(t, err)
+		})
+	}
+}
---
+++ backend/internal/domain/test/handler/empty_handler.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"context"
+
+	"buf.build/gen/go/mickamy/sampay/connectrpc/go/test/v1/testv1connect"
+	testv1 "buf.build/gen/go/mickamy/sampay/protocolbuffers/go/test/v1"
+	"connectrpc.com/connect"
+)
+
+type TestHandler struct {
+	Exec func(ctx context.Context, req *connect.Request[testv1.TestRequest])
+}
+
+func (h *TestHandler) Test(
+	ctx context.Context,
+	req *connect.Request[testv1.TestRequest],
+) (*connect.Response[testv1.TestResponse], error) {
+	h.Exec(ctx, req)
+	return connect.NewResponse(&testv1.TestResponse{}), nil
+}
+
+var (
+	_ testv1connect.TestServiceHandler = (*TestHandler)(nil)
+)
