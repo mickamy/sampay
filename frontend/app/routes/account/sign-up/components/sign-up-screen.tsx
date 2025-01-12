dced8faca5406d8a1657b314b711945677f9c47d
mickamy <tetsuro@mickamy.com>
2025-01-12 15:15:46 +0900

    feat: enrich sign-up/in

--- frontend/app/routes/account/sign-up/components/sign-up-screen.tsx
+++ frontend/app/routes/account/sign-up/components/sign-up-screen.tsx
@@ -1,5 +1,7 @@
 import { useCallback } from "react";
 import { useActionData } from "react-router";
+import { Separator } from "~/components/ui/separator";
+import UnderlinedLink from "~/components/underlined-link";
 import { useJsonSubmit } from "~/hooks/use-submit";
 import type { APIError } from "~/lib/api/response";
 import type { z } from "~/lib/form/zod";
@@ -29,11 +31,26 @@ export default function SignUpScreen() {
     <>
       <div className="container mx-auto flex h-screen w-full flex-col justify-center px-12 space-y-6 sm:w-[420px] lg:p-8">
         <div className="flex flex-col space-y-2 text-center">
-          <h1 className="text-2xl font-semibold tracking-tight">
+          <h1 className="text-2xl font-semibold">
             {t("account.sign-up.title")}
           </h1>
         </div>
         <SignUpForm onSubmitData={onSubmit} error={actionData?.error} />
+        <p className="flex flex-col space-y-4 px-8 text-center text-sm text-muted-foreground">
+          <UnderlinedLink to="/terms">
+            {t("account.sign-up.terms")}
+          </UnderlinedLink>
+          <UnderlinedLink to="/privacy">
+            {t("account.sign-up.privacy")}
+          </UnderlinedLink>
+        </p>
+        <Separator />
+        <p className="flex flex-row justify-center text-sm text-muted-foreground">
+          {t("account.sign-up.have-account")}
+          <UnderlinedLink to="/auth/sign-in">
+            {t("account.sign-up.sign-in")}
+          </UnderlinedLink>
+        </p>
       </div>
     </>
   );
--- frontend/app/routes/auth/sign-in/components/sign-in-screen.tsx
+++ frontend/app/routes/auth/sign-in/components/sign-in-screen.tsx
@@ -1,5 +1,7 @@
 import { useCallback } from "react";
 import { useActionData } from "react-router";
+import { Separator } from "~/components/ui/separator";
+import UnderlinedLink from "~/components/underlined-link";
 import { useJsonSubmit } from "~/hooks/use-submit";
 import type { APIError } from "~/lib/api/response";
 import type { z } from "~/lib/form/zod";
@@ -28,11 +30,22 @@ export default function SignInScreen() {
     <>
       <div className="container mx-auto flex h-screen w-full flex-col justify-center px-12 space-y-6 sm:w-[420px] lg:p-8">
         <div className="flex flex-col space-y-2 text-center">
-          <h1 className="text-2xl font-semibold tracking-tight">
-            {t("auth.sign-in.title")}
-          </h1>
+          <h1 className="text-2xl font-semibold">{t("auth.sign-in.title")}</h1>
         </div>
         <SignInForm onSubmitData={onSubmit} error={actionData?.error} />
+        <Separator />
+        <p className="flex flex-col space-y-4 px-8 text-center text-sm text-muted-foreground">
+          <UnderlinedLink to="/auth/forgot-password">
+            {t("auth.sign-in.forgot-password")}
+          </UnderlinedLink>
+        </p>
+        <Separator />
+        <p className="flex flex-row justify-center text-sm text-muted-foreground">
+          {t("auth.sign-in.have-no-account")}
+          <UnderlinedLink to="/account/sign-up">
+            {t("auth.sign-in.sign-up")}
+          </UnderlinedLink>
+        </p>
       </div>
     </>
   );
