c91f062e7468304d5aa6e0ebf5ec7a4fb40f8106
mickamy <tetsuro@mickamy.com>
2025-01-13 14:04:43 +0900

    chore: add provider images

--- frontend/app/models/user/user-link-provider-type-model.ts
+++ frontend/app/models/user/user-link-provider-type-model.ts
@@ -1,3 +1,18 @@
 export const UserLinkProviderTypes = ["kyash", "paypay", "amazon", "other"];
 
 export type UserLinkProviderType = (typeof UserLinkProviderTypes)[number];
+
+export function getUserLinkProviderTypeImage(
+  provider: UserLinkProviderType,
+): string {
+  switch (provider) {
+    case "kyash":
+      return "/provider/kyash.png";
+    case "paypay":
+      return "/provider/paypay.jpg";
+    case "amazon":
+      return "/provider/amazon.png";
+    default:
+      return "/provider/other.svg";
+  }
+}
