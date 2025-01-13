2668d0a04b7f8340529c86df66bebb015e098c2a
mickamy <tetsuro@mickamy.com>
2025-01-13 11:27:10 +0900

    chore: add @tailwindcss/line-clamp

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -57,5 +57,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("@tailwindcss/line-clamp"), require("tailwindcss-animate")],
 } satisfies Config;
