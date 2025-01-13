3615a9eee68beae8b30fd662f072b9287874ac48
mickamy <tetsuro@mickamy.com>
2025-01-13 12:30:21 +0900

    fix: make line-clamp not stripped on build

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,6 +3,15 @@ import type { Config } from "tailwindcss";
 export default {
   darkMode: ["class"],
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    "line-clamp-1",
+    "line-clamp-2",
+    "line-clamp-3",
+    "line-clamp-4",
+    "line-clamp-5",
+    "line-clamp-6",
+    "line-clamp-none",
+  ],
   theme: {
     extend: {
       borderRadius: {
