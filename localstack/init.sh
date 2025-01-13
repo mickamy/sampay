689e1fe631ae7dc99d76765f79e555e09e39942c
mickamy <tetsuro@mickamy.com>
2025-01-13 14:18:42 +0900

    ci: run localstack image on testing backend

--- localstack/init.sh
+++ localstack/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awslocal s3api create-bucket --bucket sampay-public
+awslocal s3api create-bucket --bucket sampay-public --create-bucket-configuration LocationConstraint=ap-northeast-1
