90c5965b01e80c0f4a5e990119da8dbe033ab913
mickamy <tetsuro@mickamy.com>
2025-01-13 12:03:44 +0900

    feat: expandable bio

--- frontend/app/routes/admin/components/admin-user-profile.tsx
+++ frontend/app/routes/admin/components/admin-user-profile.tsx
@@ -1,5 +1,6 @@
 import React, { type HTMLAttributes } from "react";
 import Avatar from "~/components/avatar";
+import ExpandableText from "~/components/expandable-text";
 import UnderlinedLink from "~/components/underlined-link";
 import { useSafeTranslation } from "~/lib/i18n/hooks";
 import { cn } from "~/lib/utils";
@@ -21,9 +22,7 @@ export default function AdminUserProfile({
       <div className="mx-auto flex w-full flex-col items-center space-y-2">
         <Avatar src={profile.imageURL} className="w-24 h-24" />
         <h2 className="font-bold">{profile?.name}</h2>
-        <h2 className="text-center" style={{ whiteSpace: "pre-wrap" }}>
-          {profile.bio}
-        </h2>
+        <ExpandableText>{profile.bio}</ExpandableText>
       </div>
       <UnderlinedLink to="/admin/edit" className="text-center underline mt-4">
         {t("admin.index.edit-profile")}
