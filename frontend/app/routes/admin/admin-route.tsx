880dae3a604ce93ca22f32d5033b9f2b610b2564
mickamy <tetsuro@mickamy.com>
2025-01-13 19:47:15 +0900

    feat: update user profile except image

--- frontend/app/routes/admin/admin-route.tsx
+++ frontend/app/routes/admin/admin-route.tsx
@@ -1,11 +1,18 @@
 import { OnboardingService } from "@buf/mickamy_sampay.bufbuild_es/registration/v1/onboarding_pb";
 import { UserService } from "@buf/mickamy_sampay.bufbuild_es/user/v1/user_pb";
-import { type LoaderFunction, redirect } from "react-router";
+import { UserProfileService } from "@buf/mickamy_sampay.bufbuild_es/user/v1/user_profile_pb";
+import {
+  type ActionFunction,
+  type LoaderFunction,
+  redirect,
+} from "react-router";
+import { userProfileSchema } from "~/components/user-profile-form";
 import { withAuthentication } from "~/lib/api/request";
 import type { APIError } from "~/lib/api/response";
 import type { Either } from "~/lib/either/either";
 import { convertToUser } from "~/models/user/user-model";
 import AdminScreen, {
+  type ActionData,
   type LoaderData,
 } from "~/routes/admin/components/admin-screen";
 
@@ -37,12 +44,18 @@ export default function Admin() {
   return <AdminScreen />;
 }
 
-export const action = async ({ request }) => {
+export const action: ActionFunction = async ({ request }) => {
   switch (request.method) {
     case "PUT": {
       if (request.headers.get("content-type")?.startsWith("application/json")) {
         return handleJSONPut({ request }).then((it) => it.value);
       }
+      if (
+        request.headers.get("content-type")?.startsWith("multipart/form-data")
+      ) {
+        return handleMultipartPut({ request }).then((it) => it.value);
+      }
+      throw new Error("unsupported content type");
     }
   }
 };
@@ -52,8 +65,6 @@ async function handleJSONPut({
 }: { request: Request }): Promise<Either<Response, APIError>> {
   const body = await request.json();
   switch (body.type) {
-    case "profile":
-      return putProfile({ request, body });
     case "link":
       return putLink({ request, body });
     default:
@@ -61,12 +72,34 @@ async function handleJSONPut({
   }
 }
 
+async function handleMultipartPut({
+  request,
+}: { request: Request }): Promise<Either<Response, APIError>> {
+  const formData = await request.formData();
+  const type = formData.get("type");
+  switch (type) {
+    case "profile":
+      return putProfile({ request, body: formData });
+    default:
+      throw new Error(`unknown type: ${type}`);
+  }
+}
+
 async function putProfile({
   request,
   body,
-}: { request: Request; body: unknown }): Promise<Either<Response, APIError>> {
-  console.log("putProfile", request, body);
-  return Response.json({});
+}: { request: Request; body: FormData }): Promise<Either<Response, APIError>> {
+  return withAuthentication({ request }, async ({ getClient }) => {
+    const { image, ...data } = userProfileSchema.parse(
+      Object.fromEntries(body),
+    );
+    await getClient(UserProfileService).updateUserProfile(data);
+    const actionData: ActionData = {
+      putProfileSuccess: true,
+      putProfileError: undefined,
+    };
+    return Response.json(actionData);
+  });
 }
 
 async function putLink({
