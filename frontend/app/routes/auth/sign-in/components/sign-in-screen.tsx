eab5727977307479d6054e462c2ac472f32c7185
mickamy <tetsuro@mickamy.com>
2025-01-19 10:23:24 +0900

    lint: fix violations

--- frontend/app/routes/auth/sign-in/components/sign-in-screen.tsx
+++ frontend/app/routes/auth/sign-in/components/sign-in-screen.tsx
@@ -1,13 +1,12 @@
-import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { useActionData } from "react-router";
 import { Separator } from "~/components/ui/separator";
 import UnderlinedLink from "~/components/underlined-link";
 import { useJsonSubmit } from "~/hooks/use-submit";
 import type { APIError } from "~/lib/api/response";
-import type { z } from "~/lib/form/zod";
-import { requestEmailVerificationSchema } from "~/routes/account/sign-up/components/request-email-verification-form";
-import SignInForm from "~/routes/auth/sign-in/components/sign-in-form";
+import SignInForm, {
+  authSignInSchema,
+} from "~/routes/auth/sign-in/components/sign-in-form";
 
 export interface ActionData {
   error?: APIError;
@@ -16,13 +15,7 @@ export interface ActionData {
 export default function SignInScreen() {
   const actionData = useActionData<ActionData>();
 
-  const submit = useJsonSubmit(requestEmailVerificationSchema);
-  const onSubmit = useCallback(
-    (data: z.infer<typeof requestEmailVerificationSchema>) => {
-      submit(data);
-    },
-    [submit],
-  );
+  const submit = useJsonSubmit(authSignInSchema);
 
   const { t } = useTranslation();
 
@@ -32,7 +25,7 @@ export default function SignInScreen() {
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-2xl font-semibold">{t("auth.sign_in.title")}</h1>
         </div>
-        <SignInForm onSubmitData={onSubmit} error={actionData?.error} />
+        <SignInForm onSubmitData={submit} error={actionData?.error} />
         <Separator />
         <p className="flex flex-col space-y-4 px-8 text-center text-sm text-muted-foreground">
           <UnderlinedLink to="/auth/forgot_password">
