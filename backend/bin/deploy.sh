26e47079e4fa4ca7621507e44d31068802f22a9d
mickamy <tetsuro@mickamy.com>
2025-01-29 23:31:36 +0900

    fix: service name

--- backend/bin/deploy.sh
+++ backend/bin/deploy.sh
@@ -6,7 +6,7 @@ git pull
 
 cd backend
 make build db-prepare
-sudo systemctl restart sampay-backend
+sudo systemctl restart sampay-api
 sudo systemctl restart sampay-worker
 
 if timeout 10s bash -c 'until systemctl is-active --quiet sampay-worker; do sleep 1; done'; then
@@ -16,14 +16,14 @@ else
     exit 1
 fi
 
-echo "Waiting for sampay-backend service to become active..."
+echo "Waiting for sampay-api service to become active..."
 retry_count=0
 max_retries=3
 retry_interval=5
 
 while [ $retry_count -lt $max_retries ]; do
-    if systemctl is-active --quiet sampay-backend; then
-        echo "sampay-backend service is active."
+    if systemctl is-active --quiet sampay-api; then
+        echo "sampay-api service is active."
         if wget -q --spider http://localhost:8080/health; then
             echo "Health check passed successfully."
             exit 0
@@ -31,7 +31,7 @@ while [ $retry_count -lt $max_retries ]; do
             echo "Health check failed. Retrying..."
         fi
     else
-        echo "sampay-backend service is not yet active. Waiting..."
+        echo "sampay-api service is not yet active. Waiting..."
     fi
 
     retry_count=$((retry_count + 1))
@@ -42,5 +42,5 @@ while [ $retry_count -lt $max_retries ]; do
     fi
 done
 
-echo "Failed to verify sampay-backend service health after $max_retries attempts."
+echo "Failed to verify sampay-api service health after $max_retries attempts."
 exit 1
