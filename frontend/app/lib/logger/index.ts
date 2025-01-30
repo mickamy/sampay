74a58db89f91cc22b5a042eddbd036c1be767259
mickamy <tetsuro@mickamy.com>
2025-01-30 16:21:32 +0900

    fix: create directory if not exists

--- frontend/app/lib/logger/index.ts
+++ frontend/app/lib/logger/index.ts
@@ -5,8 +5,13 @@ import { isBrowser } from "~/lib/utils";
 
 async function init() {
   let level = process.env.NODE_ENV === "development" ? "debug" : "info";
-  if (import.meta.env.VITE_LOG_LEVEL) {
+  if (
+    typeof import.meta.env !== "undefined" &&
+    import.meta.env.VITE_LOG_LEVEL
+  ) {
     level = import.meta.env.VITE_LOG_LEVEL;
+  } else if (process.env.VITE_LOG_LEVEL) {
+    level = process.env.VITE_LOG_LEVEL;
   }
 
   if (isBrowser()) {
@@ -17,13 +22,20 @@ async function init() {
       },
     });
   }
+
   const dir = "/var/log/sampay";
   const { createWriteStream, existsSync, mkdirSync } = await import("node:fs");
-  if (!existsSync(dir)) {
-    mkdirSync(dir, { recursive: true });
+
+  try {
+    if (!existsSync(dir)) {
+      mkdirSync(dir, { recursive: true });
+    }
+  } catch (error) {
+    console.error(`failed to create log directory: ${dir}`);
+    throw error;
   }
 
-  const path = join(dir, "web.log");
+  const path = join(dir, "frontend.log");
   const fileStream = createWriteStream(path, {
     flags: "a",
   });
