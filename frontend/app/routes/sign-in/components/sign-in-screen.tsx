31db0776a4621ca972160c766a82fa4d54f7089f
mickamy <tetsuro@mickamy.com>
2025-01-31 18:17:00 +0900

    chore: remove useless empty tag

--- frontend/app/routes/sign-in/components/sign-in-screen.tsx
+++ frontend/app/routes/sign-in/components/sign-in-screen.tsx
@@ -20,26 +20,24 @@ export default function SignInScreen() {
   const { t } = useTranslation();
 
   return (
-    <>
-      <div className="container mx-auto flex h-screen w-full flex-col justify-center px-12 space-y-6 sm:w-[420px] lg:p-8">
-        <div className="flex flex-col space-y-2 text-center">
-          <h1 className="text-2xl font-semibold">{t("auth.sign_in.title")}</h1>
-        </div>
-        <SignInForm onSubmitData={submit} error={actionData?.error} />
-        <Separator />
-        <p className="flex flex-col space-y-4 px-8 text-center text-sm text-muted-foreground">
-          <UnderlinedLink to="/reset-password">
-            {t("auth.sign_in.forgot_password")}
-          </UnderlinedLink>
-        </p>
-        <Separator />
-        <p className="flex flex-row justify-center text-sm text-muted-foreground">
-          {t("auth.sign_in.have_no_account")}
-          <UnderlinedLink to="/sign-up">
-            {t("auth.sign_in.sign_up")}
-          </UnderlinedLink>
-        </p>
+    <div className="container mx-auto flex h-screen w-full flex-col justify-center px-12 space-y-6 sm:w-[420px] lg:p-8">
+      <div className="flex flex-col space-y-2 text-center">
+        <h1 className="text-2xl font-semibold">{t("auth.sign_in.title")}</h1>
       </div>
-    </>
+      <SignInForm onSubmitData={submit} error={actionData?.error} />
+      <Separator />
+      <p className="flex flex-col space-y-4 px-8 text-center text-sm text-muted-foreground">
+        <UnderlinedLink to="/reset-password">
+          {t("auth.sign_in.forgot_password")}
+        </UnderlinedLink>
+      </p>
+      <Separator />
+      <p className="flex flex-row justify-center text-sm text-muted-foreground">
+        {t("auth.sign_in.have_no_account")}
+        <UnderlinedLink to="/sign-up">
+          {t("auth.sign_in.sign_up")}
+        </UnderlinedLink>
+      </p>
+    </div>
   );
 }
