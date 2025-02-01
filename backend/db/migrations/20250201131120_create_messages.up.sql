2ac9d5fb70d983aebad2c2ed81de9751fe7bed93
mickamy <tetsuro@mickamy.com>
2025-02-01 23:04:52 +0900

    refactor: restrict sender name and content length

--- backend/db/migrations/20250201131120_create_messages.up.sql
+++ backend/db/migrations/20250201131120_create_messages.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE messages
 (
     id          CHAR(26)     NOT NULL PRIMARY KEY,
-    sender_name VARCHAR(256) NOT NULL,
+    sender_name VARCHAR(32)  NOT NULL,
     receiver_id CHAR(26)     NOT NULL,
-    content     TEXT         NOT NULL,
+    content     VARCHAR(256) NOT NULL,
     created_at  TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_messages_receiver_id FOREIGN KEY (receiver_id) REFERENCES users (id)
 );
