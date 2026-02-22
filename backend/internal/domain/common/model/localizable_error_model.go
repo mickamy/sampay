e16b4a9b07ec2bd67d19af0b39039798aefdb795
Tetsuro Mikami <tetsuro@mickamy.com>
2026-02-22 17:13:32 +0900

    feat: add common package

---
+++ backend/internal/domain/common/model/localizable_error_model.go
@@ -0,0 +1,46 @@
+package model
+
+import (
+	"context"
+	"strings"
+
+	"golang.org/x/text/language"
+
+	"github.com/mickamy/sampay/internal/misc/contexts"
+	"github.com/mickamy/sampay/internal/misc/i18n"
+)
+
+type LocalizableError struct {
+	underlying error
+	messages   []i18n.Message
+}
+
+func NewLocalizableError(underlying error) *LocalizableError {
+	return &LocalizableError{
+		underlying: underlying,
+	}
+}
+
+func (m *LocalizableError) Error() string { return m.underlying.Error() }
+
+func (m *LocalizableError) WithMessages(messages ...i18n.Message) *LocalizableError {
+	m.messages = append(m.messages, messages...)
+	return m
+}
+
+func (m *LocalizableError) Localize(lang language.Tag) string {
+	var localized string
+	for _, message := range m.messages {
+		if localized == "" {
+			localized = i18n.Localize(lang, message)
+		} else {
+			localized = strings.Join([]string{localized, i18n.Localize(lang, message)}, "\n")
+		}
+	}
+	return localized
+}
+
+func (m *LocalizableError) LocalizeContext(ctx context.Context) string {
+	lang := contexts.MustLanguage(ctx)
+	return m.Localize(lang)
+}
