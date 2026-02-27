505511714f8845123b3cd07857cece05ab88b81d
Tetsuro Mikami <tetsuro@mickamy.com>
2026-02-28 06:17:28 +0900

    feat: add event mappers with automapper and converter registrations

---
+++ backend/internal/domain/event/mapper/gen.go
@@ -0,0 +1,12 @@
+package mapper
+
+import (
+	eventv1 "github.com/mickamy/sampay/gen/event/v1"
+	"github.com/mickamy/sampay/internal/domain/event/model"
+)
+
+//go:generate go tool automapper -from=model.Event -to=eventv1.Event -output=./ -converter-pkg=../../../lib/converter
+var (
+	_ model.Event
+	_ eventv1.Event
+)
--- backend/internal/lib/converter/converter.go
+++ backend/internal/lib/converter/converter.go
@@ -1,8 +1,12 @@
 package converter
 
 import (
-	"github.com/mickamy/automapper"
+	"time"
 
+	"github.com/mickamy/automapper"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	eventv1 "github.com/mickamy/sampay/gen/event/v1"
 	userv1 "github.com/mickamy/sampay/gen/user/v1"
 )
 
@@ -10,6 +14,10 @@ func init() {
 	automapper.RegisterFromE[string, userv1.PaymentMethodType](ToPaymentMethodType)
 	automapper.RegisterFrom[*string, string](StringToPtr)
 	automapper.RegisterFrom[int, int32](Int32ToInt)
+	automapper.RegisterFrom[int32, int](IntToInt32)
+	automapper.RegisterFrom[*timestamppb.Timestamp, time.Time](TimeToTimestamppb)
+	automapper.RegisterFrom[eventv1.ParticipantStatus, string](ToV1ParticipantStatus)
+	automapper.RegisterFrom[string, eventv1.ParticipantStatus](FromV1ParticipantStatus)
 }
 
 func StringToPtr(s string) *string {
@@ -21,4 +29,38 @@ func StringToPtr(s string) *string {
 
 func Int32ToInt(i int32) int {
 	return int(i)
+}
+
+func IntToInt32(i int) int32 {
+	return int32(i) //nolint:gosec // values are reasonable small positive integers
+}
+
+func TimeToTimestamppb(t time.Time) *timestamppb.Timestamp {
+	return timestamppb.New(t)
+}
+
+func ToV1ParticipantStatus(s string) eventv1.ParticipantStatus {
+	switch s {
+	case "unpaid":
+		return eventv1.ParticipantStatus_PARTICIPANT_STATUS_UNPAID
+	case "claimed":
+		return eventv1.ParticipantStatus_PARTICIPANT_STATUS_CLAIMED
+	case "confirmed":
+		return eventv1.ParticipantStatus_PARTICIPANT_STATUS_CONFIRMED
+	default:
+		return eventv1.ParticipantStatus_PARTICIPANT_STATUS_UNSPECIFIED
+	}
+}
+
+func FromV1ParticipantStatus(s eventv1.ParticipantStatus) string {
+	switch s {
+	case eventv1.ParticipantStatus_PARTICIPANT_STATUS_UNPAID:
+		return "unpaid"
+	case eventv1.ParticipantStatus_PARTICIPANT_STATUS_CLAIMED:
+		return "claimed"
+	case eventv1.ParticipantStatus_PARTICIPANT_STATUS_CONFIRMED:
+		return "confirmed"
+	default:
+		return ""
+	}
 }
