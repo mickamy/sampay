1ef60628234b2771241ada8aa0399c82699bd645
Tetsuro Mikami <tetsuro@mickamy.com>
2026-04-05 11:57:46 +0900

    infra: add policies for getting ECR, manipulate SQS, S3 and SES

--- infra/iam.tf
+++ infra/iam.tf
@@ -6,6 +6,51 @@ resource "aws_iam_policy" "pi_secret_policy" {
     Version = "2012-10-17"
     Statement = [
       {
+        Effect   = "Allow"
+        Action   = "ecr:GetAuthorizationToken"
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+        ]
+        Resource = [
+          aws_ecr_repository.backend.arn,
+          aws_ecr_repository.frontend.arn,
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket",
+        ]
+        Resource = [
+          aws_s3_bucket.public.arn,
+          "${aws_s3_bucket.public.arn}/*",
+          aws_s3_bucket.private.arn,
+          "${aws_s3_bucket.private.arn}/*",
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:SendMessage",
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes",
+        ]
+        Resource = [
+          aws_sqs_queue.worker.arn,
+          aws_sqs_queue.worker_dlq.arn,
+        ]
+      },
+      {
         Action   = ["secretsmanager:GetSecretValue", "secretsmanager:DescribeSecret"]
         Effect   = "Allow"
         Resource = [
@@ -13,6 +58,14 @@ resource "aws_iam_policy" "pi_secret_policy" {
           aws_secretsmanager_secret.db.arn,
           aws_secretsmanager_secret.kvs.arn,
         ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ses:SendEmail",
+          "ses:SendRawEmail",
+        ]
+        Resource = "*"
       }
     ]
   })
